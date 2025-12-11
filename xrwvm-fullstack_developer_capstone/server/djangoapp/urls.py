7fc83a0d89b4e15c6d94c48c9e248fefd1178503
Srivalli Nalla <159496919+srivallinalla12@users.noreply.github.com>
2025-12-10 19:05:55 -0500

    Add files via upload

---
+++ xrwvm-fullstack_developer_capstone/server/djangoapp/urls.py
@@ -0,0 +1,72 @@
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+app_name = 'djangoapp'
+
+urlpatterns = [
+    # path for registration
+    path(
+        route='register',
+        view=views.registration,
+        name='register',
+    ),
+
+    # path for login
+    path(
+        route='login',
+        view=views.login_user,
+        name='login',
+    ),
+
+    # path for logout
+    path(
+        route='logout',
+        view=views.logout_user,
+        name='logout',
+    ),
+
+    # path for get_cars
+    path(
+        route='get_cars',
+        view=views.get_cars,
+        name='getcars',
+    ),
+
+    # path for get_dealership
+    path(
+        route='get_dealers',
+        view=views.get_dealerships,
+        name='get_dealers',
+    ),
+    path(
+        route='get_dealers/<str:state>',
+        view=views.get_dealerships,
+        name='get_dealers_by_state',
+    ),
+
+    # path for get_dealer_details
+    path(
+        route='dealer/<int:dealer_id>',
+        view=views.get_dealer_details,
+        name='dealer_details',
+    ),
+
+    # path for get_dealer_reviews
+    path(
+        route='reviews/dealer/<int:dealer_id>',
+        view=views.get_dealer_reviews,
+        name='dealer_reviews',
+    ),
+
+    # path for add_review
+    path(
+        route='add_review',
+        view=views.add_review,
+        name='add_review',
+    ),
+] + static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
